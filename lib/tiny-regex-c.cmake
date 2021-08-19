5f8f5b01bada9ae03912f51dd19b68a6321ea5ff
dotandl <oskarzawidlak@gmail.com>
2021-08-19 22:07:10 +0200

    Add PCRE library
    Library-hoping, part 2.

--- lib/tiny-regex-c.cmake
+++
@@ -1,9 +0,0 @@
-# +----------------------------------------------------------+
-# | libgetter - part of Getter Package Manager               |
-# | Licensed under the GNU GPLv3 License. See LICENSE.txt    |
-# |                                                          |
-# | Copyright (C) 2021 dotandl                               |
-# +----------------------------------------------------------+
-
-add_library(tiny-regex-c STATIC tiny-regex-c/re.c tiny-regex-c/re.h)
-target_include_directories(tiny-regex-c PUBLIC ${PROJECT_SOURCE_DIR}/lib/tiny-regex-c)
--- src/release/release.c
+++ src/release/release.c
@@ -7,19 +7,34 @@
  * +----------------------------------------------------------+
  */
 
+#define PCRE2_CODE_UNIT_WIDTH 8
+
 #include <getter/release/release.h>
-#include <re.h>
+#include <pcre2.h>
 
 GttRelease *gtt_release_new(const char *platform, const char *arch,
                             const char *version) {
   GttRelease *self;
-  re_t regexp;
-  int match_idx, matched_elements;
+  pcre2_code *regexp;
+  pcre2_match_data *match_data;
+  PCRE2_SIZE error_offset;
+  int error_code, matches;
 
-  regexp = re_compile("[0-9]+\\.[0-9]+\\.[0-9]+");
-  match_idx = re_matchp(regexp, version, &matched_elements);
+  // Regexp checking if version field contains MAJOR.MINOR.PATCH substring.
+  regexp = pcre2_compile("[0-9]+\\.[0-9]+\\.[0-9]+", PCRE2_ZERO_TERMINATED, 0,
+                         &error_code, &error_offset, NULL);
 
-  if (match_idx == -1) return NULL;  // invalid version
+  if (regexp == NULL) return NULL;  // could not compile regexp
+
+  match_data = pcre2_match_data_create_from_pattern(regexp, NULL);
+  matches =
+      pcre2_match(regexp, version, strlen(version), 0, 0, match_data, NULL);
+
+  if (matches < 0) {
+    pcre2_match_data_free(match_data);
+    pcre2_code_free(regexp);
+    return NULL;  // version not matched
+  }
 
   self = malloc(sizeof(GttRelease));
   memset(self, 0, sizeof(GttRelease));
@@ -31,6 +46,9 @@ GttRelease *gtt_release_new(const char *platform, const char *arch,
   strcpy((char *)self->platform, platform);
   strcpy((char *)self->arch, arch);
   strcpy((char *)self->version, version);
+
+  pcre2_match_data_free(match_data);
+  pcre2_code_free(regexp);
 
   return self;
 }
