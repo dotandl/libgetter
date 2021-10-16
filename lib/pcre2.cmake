2a6c2546b8d696b5f9299fff931e58c36a12d190
dotandl <oskarzawidlak@gmail.com>
2021-10-16 17:59:24 +0200

    Fix `pcre2.h: no such file or directory`
    PCRE2 library does not set configure any include directories for targets
    by default. My Arch Linux install does have `pcre2` package installed
    so I haven't experienced any problems so far

--- lib/pcre2.cmake
+++ lib/pcre2.cmake
@@ -8,3 +8,15 @@
 set(PCRE2_BUILD_TESTS OFF)
 
 add_subdirectory(pcre2)
+
+set(PCRE2_TARGETS
+  pcre2-8-static
+  pcre2-8-shared
+)
+
+foreach(TARGET ${PCRE2_TARGETS})
+  target_include_directories(${TARGET}
+    PUBLIC "${PCRE2_BINARY_DIR}" # for pcre2.h (generated from pcre2.h.in)
+    PUBLIC "${PCRE2_SOURCE_DIR}/src" # for other internal .h files
+  )
+endforeach()
