aa37bae4191a082e26b9a495215a8556021166af
dotandl <oskarzawidlak@gmail.com>
2022-01-29 13:27:09 +0100

    Add GttReleaseInfo reader from zip

---
+++ include/getter/files/zip/release_info.h
@@ -0,0 +1,32 @@
+/*
+ * +----------------------------------------------------------+
+ * | libgetter - part of Getter Package Manager               |
+ * | Licensed under the GNU GPLv3 License. See LICENSE.txt    |
+ * |                                                          |
+ * | Copyright (C) 2021 dotandl                               |
+ * +----------------------------------------------------------+
+ */
+
+#ifndef INCLUDE_GETTER_FILES_ZIP_RELEASE_INFO_H_
+#define INCLUDE_GETTER_FILES_ZIP_RELEASE_INFO_H_
+
+GTT_HEADER_BEGIN
+
+#include <getter/release/info.h>
+#include <getter/release/release.h>
+#include <zip.h>
+
+/**
+ * Reads the GetterRelease.json file of `release` from the `zip`.
+ *
+ * \param zip Zip Box to read the GttReleaseInfo from.
+ * \param release Release to retrieve info
+ * \returns Representation of GetterRelease.json read from the zip Box.
+ * \see GttReleaseInfo
+ */
+GTT_ERROR_EMITTER GTT_API GttReleaseInfo *gtt_zip_read_release_info(
+    zip_t *zip, GttRelease *release);
+
+GTT_HEADER_END
+
+#endif /* INCLUDE_GETTER_FILES_ZIP_RELEASE_INFO_H_ */
---
+++ src/files/zip/release_info.c
@@ -0,0 +1,70 @@
+/*
+ * +----------------------------------------------------------+
+ * | libgetter - part of Getter Package Manager               |
+ * | Licensed under the GNU GPLv3 License. See LICENSE.txt    |
+ * |                                                          |
+ * | Copyright (C) 2021 dotandl                               |
+ * +----------------------------------------------------------+
+ */
+
+#include <getter/files/zip/release_info.h>
+#include <getter/tools/error.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+GttReleaseInfo *gtt_zip_read_release_info(zip_t *zip, GttRelease *release) {
+  char ri_json_path[GTT_BUFLEN];
+  int status;
+  zip_stat_t file_stat;
+  zip_file_t *file;
+  size_t bufsize;
+  char *buf;
+  GttReleaseInfo *ri;
+
+  if (zip == NULL) {
+    gtt_error(GTT_ZIP_ERROR, "Zip object is NULL");
+    return NULL;
+  }
+
+  snprintf(ri_json_path, GTT_BUFLEN, "Releases/%s/%s/%s/GetterRelease.json",
+           release->version, release->platform, release->arch);
+
+  status = zip_stat(zip, ri_json_path, 0, &file_stat);
+  if (status) {
+    gtt_error(GTT_ZIP_ERROR,
+              "Error reading info about GetterRelease.json file");
+    return NULL;
+  }
+
+  file = zip_fopen(zip, ri_json_path, 0);
+  if (file == NULL) {
+    gtt_error(GTT_ZIP_ERROR,
+              "Error opening GetterRelease.json file within zip Box");
+    return NULL;
+  }
+
+  bufsize = file_stat.size + 1;
+  buf = calloc(bufsize, sizeof(char));
+
+  status = zip_fread(zip, buf, bufsize - 1);
+  if (status < 0) {
+    gtt_error(
+        GTT_ZIP_ERROR,
+        "Error reading contents of GetterRelease.json file withing zip Box");
+    zip_fclose(file);
+    free(buf);
+    return NULL;
+  }
+
+  zip_fclose(file);
+  free(buf);
+
+  ri = gtt_release_info_new_from_json(buf);
+  if (GTT_FAILED) {
+    /* Pass the error on */
+    return NULL;
+  }
+
+  gtt_ok();
+  return ri;
+}
